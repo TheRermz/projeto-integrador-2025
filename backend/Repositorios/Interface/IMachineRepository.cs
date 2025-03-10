60b5be5d20963b142f801deaa04981ab984fa1f4
CamilaCPG <camila.cpguedes@gmail.com>
2025-03-10 17:39:48 -0300

    Continuação da criação da MachineModel + Repositorio e Interface

---
+++ backend/Repositorios/Interface/IMachineRepository.cs
@@ -0,0 +1,13 @@
+﻿using api_mrp.Objects.Models;
+
+namespace api_mrp.Repositorios.Interface
+{
+    public interface IMachineRepository
+    {
+        Task<List<MachinesModel>> GetAllMachineModels();
+        Task<MachinesModel> GetMachines(int id);
+        Task<MachinesModel> AddMachines(MachinesModel machines);
+        Task<MachinesModel> DefineFunctions(MachinesModel machines);
+        Task<bool> DeleteMachines(int id);
+    }
+}
